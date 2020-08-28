496e34efd0a38916122689196c5190461db65712
root <root@LAPTOP-1CH4ASEF.localdomain>
2020-08-28 16:21:32 +1000

    Add basic API and routing

--- jinx_project/sign_up/admin.py
+++ jinx_project/sign_up/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Sign_Up
+from .models import SignUp
 
 # Register defined models here so that they can be updated through
 # the admin page
 
-class Sign_Up_Admin(admin.ModelAdmin):
+class SignUpAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'password')
 
-admin.site.register(Sign_Up, Sign_Up_Admin)
+admin.site.register(SignUp, SignUpAdmin)
 
--- jinx_project/sign_up/models.py
+++ jinx_project/sign_up/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # TODO: Look into Django's user authentication. This will do for now
 
-class Sign_Up(models.Model):
+class SignUp(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=80)
     email = models.EmailField(max_length=250)
