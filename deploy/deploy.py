7dcffb7689e88a92afd9ceb98017bcc2c2ab89a6
John Li <lijohn161@gmail.com>
2020-09-06 21:19:28 +1000

    Improved deploy script

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -4,30 +4,69 @@
 # of getting the path of a bash script
 
 
+import logging
 import pathlib
 import subprocess
+import re
+from time import sleep
 
-curr_dir = pathlib.Path(__file__).parent.absolute()
 
-# build and deploy react front end
-print('Deploying frontend')
-subprocess.run(
-    ['docker-compose', 'up', '--build'],
-    cwd=curr_dir / 'react',
-    check=True
-    )
+def main():
+    curr_dir = pathlib.Path(__file__).parent.absolute()
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-print('Deploying backend')
-# build and deploy django backend
-subprocess.run(
-    ['docker-compose', 'up', '--detach', '--build'],
-    cwd=curr_dir / 'django',
-    check=True
-    )
+    # build and deploy react front end
+    logging.info('Deploying frontend')
+    subprocess.run(
+        ['docker-compose', 'up', '--build'],
+        cwd=curr_dir / 'react',
+        check=True
+        )
 
-print('Running database migrations')
-# run database migrations
-subprocess.run(
-    ['docker', 'exec', '-it', 'django', 'python', 'manage.py', 'migrate'],
-    check=True
-    )
+    # check to see if environments file exists
+    env_file_path = curr_dir / 'django' / '.env'
+    if not env_file_path.is_file():
+        logging.error('Expected environment file .env at %s', str(env_file_path))
+        return
+
+    # check to see if the proper keys are in the environment file
+    # does a simple regex, file could still have syntax errors!
+    with open(env_file_path, 'r') as file:
+        data = file.read()
+        if not re.search(r'^POSTGRES_PASSWORD', data, re.MULTILINE):
+            logging.error('Missing POSTGRES_PASSWORD key in environment file')
+            return
+        if not re.search(r'^DJANGO_SECRET', data, re.MULTILINE):
+            logging.error('Missing DJANGO_SECRET key in environment file')
+            return
+
+    # build and deploy django backend
+    logging.info('Deploying backend')
+    subprocess.run(
+        ['docker-compose', 'up', '--detach', '--build'],
+        cwd=curr_dir / 'django',
+        check=True
+        )
+
+    # wait until django container is up
+    for _ in range(0,60):
+        running_containers = subprocess.check_output(
+            ['docker', 'ps', '--filter', 'status=running', '--format', r'{{.Names}}']
+        )
+        if re.search('django', running_containers):
+            break
+        sleep(1)
+    else:
+        logging.error('Django could not be started !!!')
+        return
+
+    logging.info('Running database migrations')
+    # run database migrations
+    subprocess.run(
+        ['docker', 'exec', '-it', 'django', 'python', 'manage.py', 'migrate'],
+        check=True
+        )
+
+
+if __name__ == '__main__':
+    main()
