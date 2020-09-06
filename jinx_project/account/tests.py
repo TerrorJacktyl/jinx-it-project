42dfc2177db21cfd63ebac35d60b3f29c51bab5a
Jack Zezula <jayz2398@gmail.com>
2020-09-06 23:16:50 +1000

    Write basic test

--- jinx_project/account/tests.py
+++ jinx_project/account/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from .models import Account
+
 # Create your tests here.
+
+
+class UserModelTests(TestCase):
+    def test_user_creation(self):
+        """Check that user is created and stored in the database."""
+        user = User(username='jack', password='jack')
+        user.save()
+        self.assertIsNotNone(User.objects.all())
