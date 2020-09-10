48e64f5b5df52ebb3f5c7a8c4a9a879471f76d6e
John Li <lijohn161@gmail.com>
2020-09-10 11:41:06 +1000

    Improve portfolio models
    The reverse relationships default names have been changed to a more readable
    name. Each model can now also look up who the owner is.

--- jinx_project/portfolio/models.py
+++ jinx_project/portfolio/models.py
@@ -4,22 +4,36 @@ from account.models import Account
 
 class Portfolio(models.Model):
     # Link portfolio to account
-    owner = models.ForeignKey(Account, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name='portfolios')
     # Portfolio name e.g. professional, art
     name = models.CharField(max_length=100)
 
 
 class Page(models.Model):
-    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(
+        Portfolio, on_delete=models.CASCADE, related_name='pages')
     name = models.CharField(max_length=100)
     # page number (distinct from its id) to allow reordering of pages
     number = models.IntegerField(default=0)
 
+    # don't add owner as a field of page as that goes againt relational db
+    # normalisation principles
+    @property
+    def owner(self):
+        return self.portfolio.owner
+
 
 class Section(models.Model):
-    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    page = models.ForeignKey(
+        Page, on_delete=models.CASCADE, related_name='sections')
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=1000)
+
+    # not a field for the same reasoning as Page
+    @property
+    def owner(self):
+        return self.page.owner
 
 
 class MediaSection(Section):
