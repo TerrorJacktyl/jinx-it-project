0e1a3110a8f723c958978c990ee88b8b56a90afb
John Li <lijohn161@gmail.com>
2020-09-12 21:01:16 +1000

    Remove trailing slash from endpoints

--- jinx_project/portfolio/urls.py
+++ jinx_project/portfolio/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 
 urlpatterns = [
-    path('portfolios/',
+    path('portfolios',
          views.PortfolioList.as_view()),
-    path('portfolios/<portfolio_id>/',
+    path('portfolios/<portfolio_id>',
          views.PortfolioDetail.as_view()),
-    path('portfolios/<portfolio_id>/pages/',
+    path('portfolios/<portfolio_id>/pages',
          views.PageList.as_view()),
-    path('portfolios/<portfolio_id>/pages/<page_id>/',
+    path('portfolios/<portfolio_id>/pages/<page_id>',
          views.PageDetail.as_view()),
     path('portfolios/<portfolio_id>/pages/<page_id>/sections',
          views.SectionList.as_view()),
