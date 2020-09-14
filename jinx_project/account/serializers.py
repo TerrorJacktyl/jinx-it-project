670d13bc7564b0f101793cb87fa501f7010b016a
John Li <lijohn161@gmail.com>
2020-09-14 20:44:44 +1000

    Improve account API

--- jinx_project/account/serializers.py
+++ jinx_project/account/serializers.py
@@ -7,10 +7,11 @@ from .models import Account
 
 class AccountSerializer(serializers.HyperlinkedModelSerializer):
     # Serialize the associated user (rather than just returning the primary key for the account's user)
-    user = UserSerializer()
+    user = UserSerializer(read_only=True)
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
 
     class Meta:
         model = Account
-        fields = ['user']
-        read_only_fields = ['user']
+        fields = ['user', 'first_name', 'last_name']
         depth = 1
--- jinx_project/account/swagger.py
+++ jinx_project/account/swagger.py
@@ -3,4 +3,4 @@ from drf_yasg.inspectors import SwaggerAutoSchema
 
 class AccountAutoSchema(SwaggerAutoSchema):
     def get_tags(self, operation_keys=None):
-        return ['Account']
+        return ['account']
