9293d679993c93af9814e5e199160fce04cddb78
John Li <lijohn161@gmail.com>
2020-09-15 12:55:56 +1000

    Add some basic integration tests

--- jinx_project/portfolio/tests.py
+++ jinx_project/portfolio/tests.py
@@ -1,49 +1,65 @@
-from django.test import TestCase
+from hypothesis import given, settings, strategies as st
+from hypothesis.extra.django import TestCase, from_model
 
-from .models import Portfolio, Page, Section
-from account.models import Account
+from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework.test import APIClient, APIRequestFactory, force_authenticate
+
+from account.models import Account
+
+from .models import Portfolio, Page, TextSection
+from . import views
 
 
-class PortfolioModelTests(TestCase):
+def generate_with_pages(portfolio: Portfolio) -> Portfolio:
+    return st.lists(from_model(Page, portfolio=st.just(portfolio))).map(lambda _: portfolio)
 
+
+class PortfolioTest(TestCase):
     def setUp(self):
-        """Code run before each test. Create and link a portfolio to an existing account"""
-        self.user = User.objects.create(username='jack', password='jack')
-        self.account = Account.objects.create(user=self.user, first_name="Jack",
-                                              last_name="OLantern", email="jacko@lanter.ns")
-        self.portfolio = Portfolio.objects.create(
-            owner=self.account, name="My First Portfolio")
+        """Code run before each test. Setup API access simulation."""
+        pass
 
-    def test_portfolio_creation(self):
-        """Tests the setUp code."""
-        self.assertIsNotNone(Portfolio.objects.all())
+    # test for max of 2 seconds with 10 random examples
+    @settings(deadline=2000, max_examples=10)
+    # generate a random user and random portfolio name
+    @given(user=from_model(User), name=st.text())
+    def test_create(self, user: User, name: str):
+        client = APIClient()
+        # generate a random name
+        data = {'name': name}
+        # we'll assume that djoser has no authentication bugs and just bypass the login system
+        # https://www.django-rest-framework.org/api-guide/testing/#forcing-authentication
+        client.force_authenticate(user=user)
+        response = client.post(
+            reverse('portfolio_list'),
+            data
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(
+            response.data, {'owner': user.pk, 'name': name, 'pages': []})
 
-    def test_add_page(self):
-        """Add a page to the portfolio."""
-        self.page = self.portfolio.page_set.create(
-            name="Home page",
-            number=1,
-        )
-        # check that we can access the new page from the portfolio
-        self.assertIsNotNone(self.portfolio.page_set)
 
-    def test_add_section(self):
-        """Add multiple sections to the page."""
-        self.test_add_page()
-        self.section_about_me = self.page.section_set.create(
-            name="About me",
-            page=self.page,
-            description="*looks at bugs* omaewamoushindeiru",
+    @settings(deadline=2000, max_examples=10)
+    @given(portfolio=from_model(Portfolio, owner=from_model(User)).flatmap(generate_with_pages))
+    def test_retrieve(self, portfolio):
+        client = APIClient()
+        # autenticate as portfolio owner
+        client.force_authenticate(user=portfolio.owner)
+        # try to get the portfolio
+        response = client.get(
+            reverse(
+                'portfolio_detail',
+                kwargs={
+                    'portfolio_id': portfolio.pk
+                }
+            )
         )
-        self.section_history = self.page.section_set.create(
-            name="Professional history",
-            page=self.page,
-            description="NANI",
-        )
-        # check that both new sections were added and are accessible from the page
-        self.assertIsNotNone(self.page.section_set)
-        # None check before size check
-        self.assertEqual(len(self.page.section_set.all()), 2)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {
+            'id': portfolio.pk,
+            'owner': portfolio.owner.pk,
+            'name': portfolio.name,
+            'pages': list(map(lambda p: p.pk,portfolio.pages.all())),
+        })
