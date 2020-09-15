665b512c524e916014b31189f5f92fbccdf52fbc
John Li <lijohn161@gmail.com>
2020-09-15 22:04:22 +1000

    Add additional tests

--- jinx_project/account/tests.py
+++ jinx_project/account/tests.py
@@ -1,4 +1,7 @@
-from django.test import TestCase
+from hypothesis import given, settings, strategies as st
+from hypothesis.extra.django import TestCase, from_model, from_field
+
+from rest_framework.test import APIClient, APIRequestFactory, force_authenticate
 
 from django.contrib.auth.models import User
 from .models import Account
@@ -6,15 +9,53 @@ from .models import Account
 # Create your tests here.
 
 
-class UserModelTests(TestCase):
-    def test_plain_user_creation(self):
-        """Check that a (built-in Django) user is created."""
-        User.objects.create(username='jack', password='jack')
-        self.assertIsNotNone(User.objects.all())
+class AccountTest(TestCase):
+    # this does not test failure cases
+    # generates valid inputs to the given fields
+    @given(
+        first_name=from_field(User.first_name),
+        last_name=from_field(User.last_name),
+        email=from_field(User.email),
+        username=from_field(User.username),
+        password=from_field(User.password)
+    )
+    def test_account_creation(self, first_name, last_name, email, username, password):
+        client = APIClient()
+        with self.subTest(msg='register'):
+            response = client.post('/auth/users', {
+                'username': username,
+                'password': password,
+                'email': email,
+            })
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.data['username'], username)
+            self.assertEqual(response.data['email'], email)
 
-    def test_full_user_creation(self):
-        """Check that a (built-in Django) user is created with a corresponding account."""
-        user = User.objects.create(username='jack', password='jack')
-        Account.objects.create(user=user, first_name="Jack",
-                               last_name="OLantern", email="jacko@lanter.ns")
-        self.assertIsNotNone(User.objects.all())
+        with self.subTest(msg='login'):
+            response = client.post('/auth/token/login', {
+                'username': username,
+                'password': password,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('auth_token' in response.data)
+
+            token = response.data['auth_token']
+            self.assertTrue(len(token) > 0)
+
+        with self.subTest(msg='update name'):
+            client.credentials(HTTP_AUTHORIZATION='Token: ' + token)
+            response = client.put('/api/accounts/me', {
+                'first_name': first_name,
+                'last_name': last_name,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data['first_name'], first_name)
+            self.assertEqual(response.data['last_name'], last_name)
+
+        with self.subTest(msg='GET details'):
+        response = client.get('/api/accounts/me')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['first_name'], first_name)
+        self.assertEqual(response.data['last_name'], last_name)
+        self.assertEqual(response.data['user']['email'], email)
+        self.assertEqual(response.data['user']['username'], username)
--- jinx_project/portfolio/tests.py
+++ jinx_project/portfolio/tests.py
@@ -12,8 +12,16 @@ from .models import Portfolio, Page, TextSection
 from . import views
 
 
-def generate_with_pages(portfolio: Portfolio) -> Portfolio:
+def generate_portfolio() -> st.SearchStrategy:
+    return from_model(Portfolio, owner=from_model(User))
+
+
+def add_pages_to_portfolio(portfolio: Portfolio) -> Portfolio:
     return st.lists(from_model(Page, portfolio=st.just(portfolio))).map(lambda _: portfolio)
+
+
+def add_sections_to_page(page: Page) -> Page:
+    return st.lists(from_model(TextSection, page=st.just(page))).map(lambda _: page)
 
 
 class PortfolioTest(TestCase):
@@ -36,14 +44,15 @@ class PortfolioTest(TestCase):
             reverse('portfolio_list'),
             data
         )
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(
-            response.data, {'owner': user.pk, 'name': name, 'pages': []})
-
+        if len(name) == 0 or len(name) > 100:
+            self.assertEqual(response.status_code, 400)
+        else:
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.data['name'], name)
 
     @settings(deadline=2000, max_examples=10)
-    @given(portfolio=from_model(Portfolio, owner=from_model(User)).flatmap(generate_with_pages))
-    def test_retrieve(self, portfolio):
+    @given(portfolio=generate_portfolio().flatmap(add_pages_to_portfolio))
+    def test_retrieve(self, portfolio: Portfolio):
         client = APIClient()
         # autenticate as portfolio owner
         client.force_authenticate(user=portfolio.owner)
@@ -61,5 +70,49 @@ class PortfolioTest(TestCase):
             'id': portfolio.pk,
             'owner': portfolio.owner.pk,
             'name': portfolio.name,
-            'pages': list(map(lambda p: p.pk,portfolio.pages.all())),
+            'pages': list(map(lambda p: p.pk, portfolio.pages.all())),
+        })
+
+
+class PageTest(TestCase):
+    @settings(deadline=2000, max_examples=10)
+    @given(portfolio=generate_portfolio(), name=st.text())
+    def test_create(self, portfolio, name):
+        client = APIClient()
+        client.force_authenticate(portfolio.owner)
+
+        # TODO, generate random numbers for the number within the valid range
+        data = {'name': name, 'number': 0}
+        response = client.post(
+            reverse('page_list', kwargs={'portfolio_id': portfolio.pk}),
+            data
+        )
+        if len(name) == 0 or len(name) > 100:
+            self.assertEqual(response.status_code, 400)
+        else:
+            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.data['name'], name)
+            self.assertEqual(response.data['number'], 0)
+
+    @settings(deadline=2000, max_examples=10)
+    @given(page=from_model(Page, portfolio=generate_portfolio(), name=st.text(), number=st.just(0))
+           .flatmap(add_sections_to_page))
+    def test_retrieve(self, page: Page):
+        client = APIClient()
+        client.force_authenticate(page.owner)
+        response = client.get(
+            reverse(
+                'page_detail',
+                kwargs={
+                    'portfolio_id': page.portfolio.pk,
+                    'page_id': page.pk,
+                }
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {
+            'id': page.pk,
+            'name': page.name,
+            'number': page.number,
+            'sections': list(map(lambda p: p.pk, page.sections.all())),
         })
