66342af3ae73df9066196dc8d904a39dc30bfb98
Jack Z <jackzezula@tuta.io>
2020-09-17 00:53:14 +1000

    Working client tests
    Create separate tests module in `jinx_project` (backend) for writing more wholistic tests.
    Add `test_client.py` for testing user creation, authenticated login, portfolio and friends creation.

--- jinx_project/tests/test_client.py
+++ jinx_project/tests/test_client.py
@@ -5,6 +5,8 @@ from rest_framework.renderers import JSONRenderer
 from django.contrib.auth.models import User
 from portfolio.models import Portfolio, Page, Section
 
+from django.urls import reverse
+
 
 class ClientTest(TestCase):
     def setUp(self):
@@ -18,9 +20,10 @@ class ClientTest(TestCase):
 
         # Apparently .login doesn't authenticate - manually set header
         token = self.client.post(
-            '/auth/token/login', {'username': 'gavin',
-                                  'password': 'dontJinxIt'},
-            format='json').json()['auth_token']
+            '/auth/token/login',
+            data={"username": "gavin", "password": "dontJinxIt"},
+            format="json").json()['auth_token']
+
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
 
         # # Check login successful - this makes the PUT not work below
@@ -30,21 +33,48 @@ class ClientTest(TestCase):
         self.client.put('/api/accounts/me',
                         {'first_name': 'Gavin', 'last_name': 'The Great'})
         print(self.client.get('/api/accounts/me').json())
-        # Force CTRF auth
-        self.client.force_authenticate(user=User.objects.get(username='gavin'))
 
     def test_portfolio_creation(self):
         # Create a portfolio for an authenticated user, and add some content
         # Result doesn't save here, but it works over swagger with this curl:
         # curl -X POST "http://localhost:8080/api/portfolios" -H  "accept: application/json" -H  "Authorization: Token 2426e80a17d532a6f6a3d71b1637e4f1ac71218d" -H  "Content-Type: application/json" -H  "X-CSRFToken: 27hn7JEIrmmG6g4RZYeuYOjBX459j7YvxlkI0GemBq6rSZcya5AXSycE1f4pPqBk" -d "{  \"name\": \"Software\"}"
 
+        # reverse('portfolio_list') returns '/api/portfolios'
+        # but using the latter string doesn't (always) work
         self.client.post(
-            '/api​/portfolios', {'name': 'Software Engineering'}, format='json'
+            '/api/portfolios',
+            data={"name": "Software"},
+            format='json'
         )
-        portfolio_id = self.client.get('/api/portfolios').json()
-        print(portfolio_id)
+        portfolio_id = self.client.get('/api/portfolios').json()[0]['id']
 
-        # Create some pages if you can get the above to work
+        # Create some pages
+        self.client.post(
+            reverse('page_list', kwargs={'portfolio_id': portfolio_id}),
+            {"name": "About me", "number": 0}  # number must start at 0
+        )
+        self.client.post(
+            reverse('page_list', kwargs={'portfolio_id': portfolio_id}),
+            {"name": "Projects", "number": 1}  # number must start at 0
+        )
+        page_id = self.client.get(
+            reverse('page_list', kwargs={'portfolio_id': portfolio_id})).json()[0]['number']
+
+        # Create some sections
+        self.client.post(
+            # reverse('section_list', kwargs={
+            #     'portfolio_id': portfolio_id,
+            #     'page_id': page_id
+            # }), # couldn't get this url to reverse properly; hardcoding
+            "/api/portfolios/" + str(portfolio_id) + \
+            "/pages/" + str(page_id) + "/sections",
+            {"name": "Bio", "number": 0, "type": "text",
+                "content": "I'm a giraffe!"},
+            format='json'
+        )
+        # Check the pages and sections were created properly
+        self.assertIsNotNone(self.client.get(
+            "/api/portfolios/1/pages/1/sections").json())
 
     def tearDown(self):
         self.client.logout()
