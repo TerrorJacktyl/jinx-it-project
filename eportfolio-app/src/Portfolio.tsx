213f91a03fabc565128e580506a1397937b9535a
Jiles <jilesdenham@gmail.com>
2020-09-17 13:32:02 +1000

    Token request now returning 200
    NOTE: CORS WHITELIST SETTINGS HAVE BEEN CHANGED. SHOULD BE CHANGED BACK AND FIXED BEFORE PR!

--- eportfolio-app/src/Portfolio.tsx
+++ eportfolio-app/src/Portfolio.tsx
@@ -91,48 +91,70 @@ const Portfolio = () => {
     username: 'yeahnah',
     password: 'yeupyeup'
   }
+  const [token, setToken] = useState(null);
   const [portfolios, setPortfolios] = useState(null);
   const [pages, setPages] = useState(null);
   const [sections, setSections] = useState(null);
-  useEffect(() => {
-//    const fetchPortfolios = async () => {
-//      const portfolios = await axios.get(
-//        `http://127.0.0.1:8080/api/portfolios`, { auth: account }
-//      );
-//      setPortfolios(portfolios);
-//    };
-//    const fetchPages = async () => {
-//      const pages = await axios.get(
-//        `http://127.0.0.1:8080/api/porfolios/${portfolios[0].id}/pages`, { auth: account }
-//      );
-//      setPages(pages);
-//    }
-//    const fetchSections = async () => {
-//      const sections = await axios.get(
-//        `http://127.0.0.1:8080/api/portfolios/'${portfolios[0].id}/pages/${pages[0].id}/sections`, { auth: account }
-//      );
-//      setSections(sections);
-//    };
-    axios
-      .get(`http://127.0.0.1:8080/api/portfolios`, { auth: account })
-      .then((response: any) => {
-        setPortfolios(response);
-        console.log(response);
-        return axios.get(`http://127.0.0.1:8080/api/portfolios/${response[0].id}/pages`, { auth: account }) 
-      })
-      .then((response: any) => {
-        setPages(response);
-        console.log(response);
-        return axios.get('http://127.0.0.1:8080/api/portfolios/' + portfolios[0].id + '/pages/' + pages[0].id + '/sections', { auth: account })
-      })
-      .then((response: any) => {
-        setSections(response);
-        console.log(response);
-      })
-      .catch((error: any) => {
-        console.log(error);
-      })
-  }, []);
+//  useEffect(() => {
+//    axios
+//      .get(`http://127.0.0.1:8080/auth/token/login`, { auth: account })
+//      .then((response: any) => {
+//        setToken(response);
+//        console.log(response);
+//        return axios.get(`http://127.0.0.1:8080/api/portfolios`, { auth: token })
+//      })
+//      .then((response: any) => {
+//        setPortfolios(response);
+//        console.log(response);
+//        return axios.get(`http://127.0.0.1:8080/api/portfolios/${response[0].id}/pages`, { auth: token }) 
+//      })
+//      .then((response: any) => {
+//        setPages(response);
+//        console.log(response);
+//        return axios.get('http://127.0.0.1:8080/api/portfolios/' + portfolios[0].id + '/pages/' + pages[0].id + '/sections', { auth: token })
+//      })
+//      .then((response: any) => {
+//        setSections(response);
+//        console.log(response);
+//      })
+//      .catch((error: any) => {
+//        console.log(error);
+//      });
+//  }, []);{
+  const fetchToken = async () => {
+    const token = await axios.post(
+      `http://127.0.0.1:8080/auth/token/login`, account
+    )
+    .catch((error: any) => {
+      console.log(error);
+    });
+    setToken(token.auth_token);
+    console.log(token);
+  }
+  const fetchPortfolios = async () => {
+    const portfolios = await axios.get(
+      `http://127.0.0.1:8080/api/portfolios`, { auth: token }
+    );
+    setPortfolios(portfolios);
+  };
+  const fetchPages = async () => {
+    const pages = await axios.get(
+      `http://127.0.0.1:8080/api/porfolios/${portfolios[0].id}/pages`, { auth: token }
+    );
+    setPages(pages);
+  }
+  const fetchSections = async () => {
+    const sections = await axios.get(
+      `http://127.0.0.1:8080/api/portfolios/'${portfolios[0].id}/pages/${pages[0].id}/sections`, { auth: token }
+    );
+    setSections(sections);
+  };
+
+  fetchToken();
+  fetchPortfolios();
+  fetchPages();
+  fetchSection();
+
   return (
     <AccountPageDiv>      
       <SiteHeader>
@@ -143,17 +165,20 @@ const Portfolio = () => {
           </HeaderTitle>
         </HeaderDiv>
       </SiteHeader>
-      {sections.map((section: Section) => <TextSection title={section.name} content={section.content} />)}
+      <StyledFormDiv>
+        <FormTitle>{page[0].name}</FormTitle>
+      </StyledFormDiv>
+      {/*{sections.map((section: Section) => <TextSection title={section.name} content={section.content} />)}*/}
     </AccountPageDiv>
 
   );
 }
 
-const TextSection: React.FC = (title: string, content: string) => (
-  <div>
-    <FieldTitle>{title}</FieldTitle>
-    <FormDiv>{content}</FormDiv>
-  </div>
-);
+//const TextSection: React.FC = (title: string, content: string) => (
+//  <div>
+//    <FieldTitle>{title}</FieldTitle>
+//    <FormDiv>{content}</FormDiv>
+//  </div>
+//);
 
 export default Portfolio;
--- jinx_project/jinx_project/settings_dev.py
+++ jinx_project/jinx_project/settings_dev.py
@@ -6,10 +6,10 @@ ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '.localhost']
 # Corsheader settings.
 # Sets which sites are allowed to contact the api
 # This should be set to where the front end will be served.
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOWED_ORIGINS = (
-    'http://localhost:3000', # do not finish with a /
-)
+CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ALLOWED_ORIGINS = (
+#    'http://localhost:3000', # do not finish with a /
+#)
 
 # Prevent browsers from sending cookies if on http
 # Disabled in dev environment as dev server will not be behind https
