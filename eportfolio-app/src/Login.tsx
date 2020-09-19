a3027043226657cc6751bd709df434016ca3b91b
Jack Z <jackzezula@tuta.io>
2020-09-20 01:00:43 +1000

    Successful login redirects to profile page

--- eportfolio-app/src/Login.tsx
+++ eportfolio-app/src/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Redirect } from 'react-router-dom';
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import {
@@ -55,69 +56,82 @@ const SignupSchema = Yup.object().shape({
 
 const Login = () => {
   const [submittionError, setSubmittionError] = useState(false);
+  const [redirect, setRedirect] = useState(false);
 
   const { login, logout } = useUser();
 
-  return (
-    <AccountPageDiv>
-      <SiteHeader>
-        <HeaderDiv>
-          <LogoLink />
-          <HeaderTitle>Login</HeaderTitle>
-        </HeaderDiv>
-      </SiteHeader>
+  const onLogin = () => {
+    return <Redirect to="/profile" />
+  }
 
-      <StyledFormDiv>
-        <FormTitle>Enter Details</FormTitle>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={SignupSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setSubmitting(true);
-            login(values.email, values.password)
-              .then(data => console.log(data))
-              .catch(response => console.error(response));
-          }}
-        >
-          {({ errors, touched, isSubmitting }) => (
-            <Form>
-              <StyledFormEntry name="email" type="email" placeholder="Email address" />
-              {errors.email && touched.email ? <ErrorMessage>{errors.email}</ErrorMessage> : null}
+  if (redirect) {
+    return onLogin();
+  }
+  else {
+    return (
+      <AccountPageDiv>
+        <SiteHeader>
+          <HeaderDiv>
+            <LogoLink />
+            <HeaderTitle>Login</HeaderTitle>
+          </HeaderDiv>
+        </SiteHeader>
 
-              <StyledFormEntry name="password" type="password" placeholder="Password" />
-              {errors.password && touched.password ? <ErrorMessage>{errors.password}</ErrorMessage> : null}
+        <StyledFormDiv>
+          <FormTitle>Enter Details</FormTitle>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={SignupSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setSubmitting(true);
+              login(values.email, values.password)
+                .then(data => {
+                  console.log(data);
+                  setRedirect(true);
+                })
+                .catch(response => console.error(response));
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => (
+              <Form>
+                <StyledFormEntry name="email" type="email" placeholder="Email address" />
+                {errors.email && touched.email ? <ErrorMessage>{errors.email}</ErrorMessage> : null}
 
-              <StyledButton
-                type="submit"
-                disabled={isSubmitting}
-                width={null}
-                textColour="#00FFC2"
-                backgroundColour={null}
-                hoverColour="#00FFC2"
-                contrastColour="#1C1C1C"
-                text="Login"
-                fontSize={null}
-              />
-              {submittionError ? <ErrorMessage>Error signing up. Please try again later.</ErrorMessage> : null}
+                <StyledFormEntry name="password" type="password" placeholder="Password" />
+                {errors.password && touched.password ? <ErrorMessage>{errors.password}</ErrorMessage> : null}
 
-              <StyledButton
-                width={null}
-                textColour="#EEEEEE"
-                backgroundColour={null}
-                hoverColour="#EEEEEE"
-                contrastColour="#1C1C1C"
-                text="Reset Password"
-                fontSize={"20px"}
-              />
+                <StyledButton
+                  type="submit"
+                  disabled={isSubmitting}
+                  width={null}
+                  textColour="#00FFC2"
+                  backgroundColour={null}
+                  hoverColour="#00FFC2"
+                  contrastColour="#1C1C1C"
+                  text="Login"
+                  fontSize={null}
+                />
+                {submittionError ? <ErrorMessage>Error signing up. Please try again later.</ErrorMessage> : null}
 
-              <StyledLink href="/signup" ><FormText>Sign up for an account</FormText></StyledLink>
+                <StyledButton
+                  width={null}
+                  textColour="#EEEEEE"
+                  backgroundColour={null}
+                  hoverColour="#EEEEEE"
+                  contrastColour="#1C1C1C"
+                  text="Reset Password"
+                  fontSize={"20px"}
+                />
 
-            </Form>
+                <StyledLink href="/signup" ><FormText>Sign up for an account</FormText></StyledLink>
 
-          )}
-        </Formik>
-      </StyledFormDiv>
-    </AccountPageDiv >)
+              </Form>
+
+            )}
+          </Formik>
+        </StyledFormDiv>
+      </AccountPageDiv >)
+  }
 }
 
 export default Login;
--- eportfolio-app/src/jinxui/hooks/useUser.tsx
+++ eportfolio-app/src/jinxui/hooks/useUser.tsx
@@ -11,30 +11,40 @@ export const useUser = () => {
     const LOGOUT_PATH = 'auth/token/logout';
     const ACCOUNT_PATH = 'api/accounts/me';
 
+    /**
+     * Abstract the login procedure. Returns the auth_token if login succeeded, 
+     * or the backend response if it failed.
+     * @param username 
+     * @param password 
+     */
     async function login(username: string, password: string): Promise<AxiosResponse<any>> {
         const response = await API.post(LOGIN_PATH, { username: username, password: password });
 
         return new Promise((resolve, reject) => {
+            // Login successful
             if ('auth_token' in response.data) {
                 resolve(response.data);
+                setState((state: IUserContext) => {
+                    return {
+                        ...state, username: username,
+                        token: response.data['auth_token'],
+                        authenticated: true,
+                    };
+                });
             }
-            else
+            // Login failed
+            else {
                 reject(response);
-        })
-
-        // .then(response => {
-        //     console.log(response);
-        //     console.log(response.data);
-        // })
-        // .catch(response => {
-        //     console.error(response);
-        // })
-        // return await response;
+            }
+        });
     }
 
     async function logout() {
-        const response = API.post(LOGOUT_PATH);
-        return await response;
+        const response = await API.post(LOGOUT_PATH);
+
+        return new Promise((resolve, reject) => {
+
+        });
     }
 
     return {
