0096822fadffa14944c900a4610245714de0c2c3
Nima Karimi <nimakarimi.it@gmail.com>
2020-09-22 15:34:19 +1000

    add to yup schema for some backend restrictions

--- eportfolio-app/src/Signup.tsx
+++ eportfolio-app/src/Signup.tsx
@@ -54,7 +54,7 @@ const SignupSchema = Yup.object().shape({
   firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
   lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
   email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string().required("Required"),
+  password: Yup.string().min(8, "Too Short!").matches(/(?!^\d+$)^.+$/, "Password cannot consist of only numbers").required("Required"),
   passwordConfirm: Yup.string()
     .oneOf([Yup.ref("password"), undefined], "Passwords don't match")
     .required("Required"),
