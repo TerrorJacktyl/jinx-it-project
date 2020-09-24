5c7c3845cdd558d4f28f0ea3e855ee9d4939be31
Jiles <jilesdenham@gmail.com>
2020-09-24 21:05:55 +1000

    Fix merge conflicts

--- eportfolio-app/src/Login.tsx
+++ eportfolio-app/src/Login.tsx
@@ -55,84 +55,6 @@ const SignupSchema = Yup.object().shape({
 });
 
 const Login = () => {
-<<<<<<< HEAD
-  const axios = require("axios").default;
-  const [submittionError, setSubmittionError] = useState(false);
-
-  return (
-    <AccountPageDiv>
-      <SiteHeader>
-        <HeaderDiv>
-          <LogoLink />
-          <HeaderTitle>Login</HeaderTitle>
-        </HeaderDiv>
-      </SiteHeader>
-
-      <StyledFormDiv>
-        <FormTitle>Enter Details</FormTitle>
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={SignupSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setSubmitting(true);
-
-            axios
-              .post(`localhost:8080/api/login`, {
-                email: values.email,
-                password: values.password,
-              })
-              .then(function (response: any) {
-                console.log(response);
-                setSubmitting(false);
-              })
-              .catch(function (error: any) {
-                setSubmittionError(true);
-                setSubmitting(false);
-                console.log(error);
-                console.log(submittionError);
-              });
-          }}
-        >
-          {({ errors, touched, isSubmitting }) => (
-            <Form>
-              <StyledFormEntry name="email" type="email" placeholder="Email address" />
-              {errors.email && touched.email ? <ErrorMessage>{errors.email}</ErrorMessage> : null}
-              
-              <StyledFormEntry name="password" type="password" placeholder="Password" />
-              {errors.password && touched.password ? <ErrorMessage>{errors.password}</ErrorMessage> : null}
-              
-              <StyledButton
-                type="submit"
-                disabled={isSubmitting}
-                width={null}
-                textColour="#00FFC2"
-                backgroundColour={null}
-                hoverColour="#00FFC2"
-                contrastColour="#1C1C1C"
-                text="Login"
-                fontSize={null}
-                action = {null}
-              />
-              {submittionError ? <ErrorMessage>Error signing up. Please try again later.</ErrorMessage> : null}
-              
-              <StyledButton
-                width={null}
-                textColour="#EEEEEE"
-                backgroundColour={null}
-                hoverColour="#EEEEEE"
-                contrastColour="#1C1C1C"
-                text="Reset Password"
-                fontSize={"20px"}
-                action = {null}
-              />
-            </Form>
-          )}
-        </Formik>
-      </StyledFormDiv>
-    </AccountPageDiv>
-  );
-};
-=======
   const [submittionError, setSubmittionError] = useState('');
   const [redirect, setRedirect] = useState(false);
 
@@ -221,6 +143,5 @@ const Login = () => {
       </AccountPageDiv >)
   }
 }
->>>>>>> nima/login
 
 export default Login;
