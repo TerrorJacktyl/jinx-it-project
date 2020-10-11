78a86d5a5b9fa10a7435e37b184ec48d23b83153
John Li <lijohn161@gmail.com>
2020-10-11 19:24:49 +1100

    Add ability to see all accounts

--- src/backend/account/urls.py
+++ src/backend/account/urls.py
@@ -4,5 +4,14 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
-    path('me', views.AccountDetail.as_view()),
+    path(
+        '',
+        views.AccountList.as_view(),
+        name='account_list',
+    ),
+    path(
+        '<account_id>',
+        views.AccountDetail.as_view(),
+        name='account_detail',
+    ),
 ]
--- src/backend/account/views.py
+++ src/backend/account/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+
 from rest_framework import generics
+from rest_framework import permissions
 
 from common.permissions import IsReadOnly
 
@@ -10,14 +12,26 @@ from .permissions import IsAccountOwner
 from .swagger import AccountAutoSchema
 
 
+class AccountList(generics.ListAPIView):
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = Account.objects.all()
+    swagger_schema = AccountAutoSchema
+
+
 class AccountDetail(generics.RetrieveUpdateAPIView):
     serializer_class = AccountSerializer
-    permission_classes = []
+    # anyone can see account details, but only the owner can update them
+    permission_classes = [IsAccountOwner | IsReadOnly]
     queryset = Account.objects.all()
 
-    # can only see own account for now
     def get_object(self):
-        obj = get_object_or_404(self.queryset, user=self.request.user)
+        account_id = self.kwargs.get('account_id')
+        if account_id == 'me':
+            obj = get_object_or_404(self.queryset, user=self.request.user)
+        else:
+            obj = get_object_or_404(self.queryset, id=account_id)
+
         self.check_object_permissions(self.request, obj)
         return obj
 
