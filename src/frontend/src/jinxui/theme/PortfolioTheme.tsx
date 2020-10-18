b33844d3d8d9f500ef93ef187f96d48f08e5e9d1
Jack Zezula <jackzezula@tuta.io>
2020-10-18 23:13:21 +1100

    Custom font in theme

--- src/frontend/src/jinxui/theme/PortfolioTheme.tsx
+++ src/frontend/src/jinxui/theme/PortfolioTheme.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
-export const MagmaTheme: Theme = createMuiTheme({
+const createTheme = (theme: any) => responsiveFontSizes(createMuiTheme(theme));
+
+export const MagmaTheme: Theme = createTheme({
     palette: {
         type: "dark",
         primary: {
@@ -13,5 +15,19 @@ export const MagmaTheme: Theme = createMuiTheme({
             "main": "#373538",
             "contrastText": "#fff"
         },
+    },
+    typography: {
+        h1: {
+            "fontFamily": "Kaushan Script"
+        },
+        h2: {
+            "fontFamily": "Kaushan Script"
+        },
+        h3: {
+            "fontFamily": "Kaushan Script"
+        },
+        h5: {
+            "fontFamily": "open sans"
+        }
     }
 })
