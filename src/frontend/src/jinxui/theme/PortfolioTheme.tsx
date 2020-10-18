597ddbd8ac1e015ca7ef5c7fed0e38f3deacbca6
Jack Zezula <jackzezula@tuta.io>
2020-10-18 23:18:29 +1100

    Add theme creation guide

--- src/frontend/src/jinxui/theme/PortfolioTheme.tsx
+++ src/frontend/src/jinxui/theme/PortfolioTheme.tsx
@@ -4,16 +4,29 @@ import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 
 const createTheme = (theme: any) => responsiveFontSizes(createMuiTheme(theme));
 
+/**
+ * Theme creation guide
+ * 1. Pick a nice primary and secondary colour: https://material.io/resources/color/
+ * 2. Create a theme object (i.e. like MagmaTheme) and type in the colours you chose as the main for 
+ *    both the primary and secondary fields.
+ * 3. Decide whether you want to use the light or dark colours from your theme. Set this as the 
+ *    `type` field in your palette.
+ * 4. Pick a nice font if you want, and link it into frontend/public/index.html. https://fonts.google.com
+ * 5. Run your theme object through `createTheme` and export it.
+ * 6. Start using it. Mui will generate a theme using a dark or light variant (depending on the 
+ *    theme type you chose) for the portfolio backgrounds, and will automatically calculate the 
+ *    contrast text colour.
+ * Happy theming!
+ */
+
 export const MagmaTheme: Theme = createTheme({
     palette: {
         type: "dark",
         primary: {
             "main": "#b71c1c",
-            "contrastText": "#000"
         },
         secondary: {
             "main": "#373538",
-            "contrastText": "#fff"
         },
     },
     typography: {
